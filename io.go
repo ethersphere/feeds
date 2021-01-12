e31aadfc14db1e83fe009d29a54d3fa975113eb8
acud <12988138+acud@users.noreply.github.com>
2021-01-12 16:17:02 +0100

    wip

---
+++ io.go
@@ -0,0 +1,18 @@
+package feeds
+
+import "context"
+
+type Loader interface {
+	// Load a reference in byte slice representation and return all content associated with the reference.
+	Load(context.Context, []byte) ([]byte, error)
+}
+
+type Saver interface {
+	// Save an arbitrary byte slice and its corresponding reference.
+	Save(context.Context, []byte, []byte) error
+}
+
+type LoadSaver interface {
+	Loader
+	Saver
+}
